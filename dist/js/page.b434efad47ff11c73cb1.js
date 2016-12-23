5a9ecd4e2fb23de88f78bb6a23ea0db46346b34f
sndraw <sndraw@github.com>
2016-12-23 14:06:22 +0800

    增加编译后文件 Signed-off-by: sndraw <sndraw@github.com>

---
+++ dist/js/page.b434efad47ff11c73cb1.js
@@ -0,0 +1,1 @@
+webpackJsonp([2],{0:function(e,exports,t){"use strict";var n=t(1),r=t(28);t(171);var a=t(172);r.render(n.createElement(a,null),document.getElementById("content"))},171:function(e,exports){},172:function(e,exports,t){"use strict";var n=t(1),r=n.createClass({displayName:"PageBox",render:function(){return n.createElement("div",{className:"page-box"},"页面")}});e.exports=r}});
