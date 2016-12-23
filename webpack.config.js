5ba9d3467b15981bfe2caaf71f02bfcc0667b4cc
sndraw <sndraw@github.com>
2016-12-23 14:09:36 +0800

    静态文件引用路径，增加dist/前缀

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     //入口文件输出配置
     output: {
         path: path.resolve(__dirname, './dist'), // 设置输出目录
-        publicPath: "/",
+        publicPath: "/dist/",
         filename: 'js/[name].[hash].js', // 输出文件名
         chunkFilename:'js/[name].[hash].js', // 按需加载模块输出文件名
     },
